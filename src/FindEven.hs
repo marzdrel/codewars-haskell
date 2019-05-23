88e2306c9b25adde48648727e1975b725b0540ff
Marz Drel <marzdrel@users.noreply.github.com>
2019-05-23 20:12:16 +0200

    Refactor FindEvent to use where/subfunction

--- src/FindEven.hs
+++ src/FindEven.hs
@@ -2,9 +2,8 @@ module FindEven where
 
 findEvenIndex :: [Int] -> Int
 findEvenIndex arr = compareList 0 0 arr
-
-compareList :: Int -> Int -> [Int] -> Int
-compareList left nstep [] = -1
-compareList left nstep (x:xs)
-  | left == sum (xs) = nstep
-  | otherwise = compareList (left + x) (nstep + 1) xs
+  where 
+    compareList left nstep [] = -1
+    compareList left nstep (x:xs)
+      | left == sum (xs) = nstep
+      | otherwise = compareList (left + x) (nstep + 1) xs
