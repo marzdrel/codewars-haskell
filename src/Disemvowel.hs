294c766043a16eae3a8fdedc7f3cba5eb8d0303c
Marz Drel <marzdrel@users.noreply.github.com>
2019-05-23 19:23:25 +0200

    Simplify implementation of Disemvowel

--- src/Disemvowel.hs
+++ src/Disemvowel.hs
@@ -1,17 +1,6 @@
 module Disemvowel where
 
+import Data.Char
+
 disemvowel :: String -> String
-disemvowel [] = []
-disemvowel x = disemvowelAcc [] "x"
-
-disemvowelAcc :: String -> String -> String
-disemvowelAcc acc [] = acc
-disemvowelAcc acc (x:xs) =
-  disemvowelAcc (acc ++ (vowFilter x "aeiouAEIOU")) xs
-
-vowFilter :: Char -> String -> String
-vowFilter char [] = [char]
-vowFilter char (x:xs)
-  | char == x = []
-  | otherwise = vowFilter char xs
-
+disemvowel = filter (flip notElem "aeiou" . toLower)
