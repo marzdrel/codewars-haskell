e8250c3b70a029660ed7d2f93df0d8b1de3a2476
Marz Drel <marzdrel@users.noreply.github.com>
2019-05-23 22:27:35 +0200

    Fix layout for Disemvowel spec

--- test/DisemvowelSpec.hs
+++ test/DisemvowelSpec.hs
@@ -3,9 +3,10 @@ module DisemvowelSpec (spec) where
 import Disemvowel (disemvowel)
 import Test.Hspec
 
-spec = do
-  it "should work for single words" $ do
-    disemvowel "hat" `shouldBe` "ht"
-    disemvowel "toast" `shouldBe` "tst"
-  it "should work with spaces" $ do
-    disemvowel "toast hat" `shouldBe` "tst ht"
+spec = 
+  describe "disemvowel" $ do
+    it "should work for single words" $ do
+      disemvowel "hat" `shouldBe` "ht"
+      disemvowel "toast" `shouldBe` "tst"
+    it "should work with spaces" $ do
+      disemvowel "toast hat" `shouldBe` "tst ht"
