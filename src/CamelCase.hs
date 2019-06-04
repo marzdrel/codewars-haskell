7a705b5a35835e096833a896abc87bfbb6918bb6
Marz Drel <marzdrel@users.noreply.github.com>
2019-06-04 20:49:10 +0200

    Add basic implementation of toCamelCase

--- src/CamelCase.hs
+++ src/CamelCase.hs
@@ -1,4 +1,11 @@
 module CamelCase where
 
+import qualified Data.List.Split as S
+import qualified Data.Char as C
+
 toCamelCase :: String -> String
-toCamelCase str = error "todo: toCamelCase"
+toCamelCase src = 
+  concat $ head list : rest
+  where
+    list = S.splitOneOf "-_" src
+    rest = map (\(x:xs) -> C.toUpper x : xs) $ tail list
