9338cb790bbf77a35ad47a061f0592d7cf0de956
Marz Drel <marzdrel@users.noreply.github.com>
2019-07-23 20:10:33 +0200

    Implement revRot

--- src/Revrot.hs
+++ src/Revrot.hs
@@ -1,5 +1,24 @@
 module Revrot where
 
+splitString :: Int -> [Char] -> [String]
+splitString 0 _ = []
+splitString _ [] = []
+splitString n xs = head : splitString n tail
+  where (head, tail) = splitAt n xs
+
+
+processString :: [Int] -> [Int]
+processString arr
+  | rem (sum $ map (^3) arr) 2 == 0 = reverse arr
+  | otherwise = shiftToLeft arr
+  where
+    shiftToLeft (x:xs) = xs ++ [x]
+
 revRot :: [Char] -> Int -> [Char]
-revRot strng sz = strng
+revRot strng sz =
+  concat $
+  map (concat . map show . processString) $
+  map (map (\x -> read [x]::Int)) $
+  filter ((== sz) . length) $
+  splitString sz strng
 
