d5ab79eeab255019563fed2f3ce40f4cebcb58fc
Marz Drel <marzdrel@users.noreply.github.com>
2019-07-25 22:04:32 +0200

    Implement Sumdivsq

--- src/Sumdivsq.hs
+++ src/Sumdivsq.hs
@@ -1,4 +1,17 @@
 module Sumdivsq where
 
 listSquared :: Int -> Int -> [(Int, Int)]
-listSquared m n = [(m, n)]
+listSquared m n =
+  filter (isSquared) $ map (squaredDivisors) [m..n]
+  where
+    isSquared (x, n) = floor (sqInt n) == ceiling (sqInt n)
+    sqInt n = sqrt $ fromIntegral n::Double
+
+getDivisorsFor i n acc
+  | i <= div n 2 && rem n i == 0 = getDivisorsFor (i + 1) n (i:acc)
+  | i <= div n 2 = getDivisorsFor (i + 1) n acc
+  | otherwise = (n:acc)
+
+squaredDivisors :: Int -> (Int, Int)
+squaredDivisors n =
+  (n, foldr ((+) . (^2)) 0 $ getDivisorsFor 1 n [])
