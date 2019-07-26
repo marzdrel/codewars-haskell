8e2fe14eb6337bf54505441fdf02b90548349d6d
Marz Drel <marzdrel@users.noreply.github.com>
2019-07-26 16:06:45 +0200

    Optimize divisors in Sumdivsq kata

--- src/Sumdivsq.hs
+++ src/Sumdivsq.hs
@@ -4,14 +4,12 @@ listSquared :: Int -> Int -> [(Int, Int)]
 listSquared m n =
   filter (isSquared) $ map (squaredDivisors) [m..n]
   where
-    isSquared (x, n) = floor (sqInt n) == ceiling (sqInt n)
-    sqInt n = sqrt $ fromIntegral n::Double
+    isSquared (_, n) = (isInt . sqrt . fromIntegral) n
+    isInt x = x == fromInteger (round x)
 
-getDivisorsFor i n acc
-  | i <= div n 2 && rem n i == 0 = getDivisorsFor (i + 1) n (i:acc)
-  | i <= div n 2 = getDivisorsFor (i + 1) n acc
-  | otherwise = (n:acc)
+divisors :: Int -> [Int]
+divisors n = filter ((0 ==) . (mod n)) [1 .. (div n 2)]
 
 squaredDivisors :: Int -> (Int, Int)
 squaredDivisors n =
-  (n, foldr ((+) . (^2)) 0 $ getDivisorsFor 1 n [])
+  (n, foldr ((+) . (^2)) 0 $ (n:divisors n))
