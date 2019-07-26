007246418e5344bf911b8354968de1cef296f157
Marz Drel <marzdrel@users.noreply.github.com>
2019-07-26 16:07:47 +0200

    Streamline Sumdivsq Kata syntax

--- src/Sumdivsq.hs
+++ src/Sumdivsq.hs
@@ -2,14 +2,9 @@ module Sumdivsq where
 
 listSquared :: Int -> Int -> [(Int, Int)]
 listSquared m n =
-  filter (isSquared) $ map (squaredDivisors) [m..n]
+  filter (isSquared) $ map (squaredDivisors) [m .. n]
   where
     isSquared (_, n) = (isInt . sqrt . fromIntegral) n
     isInt x = x == fromInteger (round x)
-
-divisors :: Int -> [Int]
-divisors n = filter ((0 ==) . (mod n)) [1 .. (div n 2)]
-
-squaredDivisors :: Int -> (Int, Int)
-squaredDivisors n =
-  (n, foldr ((+) . (^2)) 0 $ (n:divisors n))
+    divisors n = filter ((0 ==) . (mod n)) [1 .. (div n 2)]
+    squaredDivisors n = (n, foldr ((+) . (^2)) 0 $ (n:divisors n))
