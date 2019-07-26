c06bb51d1d1550eea3539db84dc8be7b08a446ed
Marz Drel <marzdrel@users.noreply.github.com>
2019-07-26 16:43:01 +0200

    Optimize divisor finding algorithm

--- src/Sumdivsq.hs
+++ src/Sumdivsq.hs
@@ -4,7 +4,8 @@ listSquared :: Int -> Int -> [(Int, Int)]
 listSquared m n =
   filter (isSquared) $ map (squaredDivisors) [m .. n]
   where
-    isSquared (_, n) = (isInt . sqrt . fromIntegral) n
+    isSquared (_, n) = isInt . sqrt . fromIntegral $ n
     isInt x = x == fromInteger (round x)
-    divisors n = filter ((0 ==) . (mod n)) [1 .. (div n 2)]
-    squaredDivisors n = (n, foldr ((+) . (^2)) 0 $ (n:divisors n))
+    squaredDivisors n = (n, foldr ((+) . (^2)) 0 $ allDivisors n)
+    divisors n = filter ((0 ==) . (mod n)) [1 .. floor . sqrt . fromIntegral $ n]
+    allDivisors n = divisors n ++ [div n i | i <- divisors n, i * i /= n]
