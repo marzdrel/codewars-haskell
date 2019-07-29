fc1a77e393b9e1e8ccc8361d84097d4ad56bd85a
Marz Drel <marzdrel@users.noreply.github.com>
2019-07-29 18:41:05 +0200

    Add better implementation of ValidPhoneNumber

--- src/ValidPhoneNumber.hs
+++ src/ValidPhoneNumber.hs
@@ -1,7 +1,15 @@
 module ValidPhoneNumber where
 
 validPhoneNumber :: String -> Bool
-validPhoneNumber number
+validPhoneNumber number =
+  map (mapDigit) number == "(111) 111-1111"
+  where
+    mapDigit char
+      | elem char ['0'..'9'] = '0'
+      | otherwise = char
+
+validPhoneNumber2 :: String -> Bool
+validPhoneNumber2 number
   | ldiff number /= 0 = False
   | otherwise = checkStringAt number
   where
