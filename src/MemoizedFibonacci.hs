1b717a60a25213260283a21cc1b2db88a3201547
Marz Drel <marzdrel@users.noreply.github.com>
2019-07-29 18:53:06 +0200

    Implement Memoized Fibonacci

--- src/MemoizedFibonacci.hs
+++ src/MemoizedFibonacci.hs
@@ -1,4 +1,8 @@
 module MemoizedFibonacci where
 
 fibonacci :: Int -> Int
-fibonacci n = error "implement"
+fibonacci = (map memoFib [0..] !!)
+  where
+    memoFib 0 = 0
+    memoFib 1 = 1
+    memoFib n = fibonacci (n-1) + fibonacci (n-2)
