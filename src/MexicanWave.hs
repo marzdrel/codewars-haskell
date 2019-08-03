682c51b188b8a0569607bfcb37b5386b88cb2269
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-03 16:44:18 +0200

    Implement MexicanWave

--- src/MexicanWave.hs
+++ src/MexicanWave.hs
@@ -1,4 +1,13 @@
 module MexicanWave where
 
+import qualified Data.Char as Char
+
 wave :: String -> [String]
-wave = error "not implemented"
+wave input = wave' input [] []
+
+wave' :: String -> String -> [String] -> [String]
+wave' [] prefix current = current
+wave' (' ':xs) prefix current = wave' xs (prefix ++ [' ']) current
+wave' (x:xs) prefix current =
+  wave' xs (prefix ++ [x]) (current ++ [prefix ++ [Char.toUpper x] ++ xs])
+
