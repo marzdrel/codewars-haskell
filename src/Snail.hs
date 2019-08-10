d061c2b022b8758824b707674b0d2112ab8e4ba2
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-10 15:31:02 +0200

    Implement Snail

--- src/Snail.hs
+++ src/Snail.hs
@@ -1,19 +1,23 @@
 module Snail where
 
-import Debug.Trace
+import Data.List
 
 snail :: [[Int]] -> [Int]
-snail = snail' []
+snail [] = []
+snail (xs:xss) = xs ++ (snail . reverse . transpose) xss
 
-snail' :: [Int] -> [[Int]] -> [Int]
-snail' acc (x:xs)
-  | trace (show x) $ null x = acc
-  | otherwise = snail' (acc ++ (outerPart (x:xs))) (innerPart xs)
+snail2 :: [[Int]] -> [Int]
+snail2 = snail2' []
+snail2' :: [Int] -> [[Int]] -> [Int]
+snail2' acc [] = acc
+snail2' acc (x:xs) = snail2' (acc ++ (outerPart (x:xs))) (innerPart xs)
   where
-    innerPart x
-      | null $ tail x = []
-      | otherwise = trace (show x) $ map (tail . init) x
-    outerPart (x:xs) = trace (show x) $ []
-      {- x ++ (map (last) (init xs)) ++ (reverse $ last xs) ++ (map (head) (init xs)) -}
-
-{- snail [[1,2,3,4],[4,5,6,1],[7,8,9,2],[1,2,3,4]] -}
+    innerPart [] = []
+    innerPart x = map (tail . init) (init x)
+    outerPart [] = []
+    outerPart (x:[]) = x
+    outerPart (x:xs) =
+      x ++
+      (map (last) (init xs)) ++
+      (reverse $ last ([]:xs)) ++
+      (reverse $ (map (head) (init xs)))
